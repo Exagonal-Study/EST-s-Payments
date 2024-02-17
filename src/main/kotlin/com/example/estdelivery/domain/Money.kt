15f521730b046cd08b701be4d143134fd5ac6820
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-02-17 16:21:48 +0900

    refactor : Money data 클래스로 변경

--- src/main/kotlin/com/example/estdelivery/domain/Money.kt
+++ src/main/kotlin/com/example/estdelivery/domain/Money.kt
@@ -3,28 +3,11 @@ package com.example.estdelivery.domain
 import jakarta.validation.constraints.PositiveOrZero
 import java.math.BigInteger
 
-class Money(
+data class Money(
     @field:PositiveOrZero
     private val amount: BigInteger
 ) {
     init {
         require(amount >= BigInteger.ZERO) { "금액은 0원 이상이어야 합니다." }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Money
-
-        return amount == other.amount
-    }
-
-    override fun hashCode(): Int {
-        return amount.hashCode()
-    }
-
-    override fun toString(): String {
-        return "Money(amount=$amount)"
     }
 }
